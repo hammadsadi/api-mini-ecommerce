4442d1c77d4d136e37d9553a8e6be04e0518a282
hammadsadi <hammad.sadi@yahoo.com>
2024-08-17 16:00:13 +0600

    Update Db Connect

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://mini-ecommerce-task.web.app"],
     credentials: true,
   })
 );
@@ -93,9 +93,8 @@ async function run() {
     });
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
