fa64ddff608a389f6f38d9f31e550f6977c3572e
hammadsadi <hammad.sadi@yahoo.com>
2024-08-18 14:28:38 +0600

    Remove Client URL From Cors Origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://mini-ecommerce-task.web.app"],
+    origin: ["https://mini-ecommerce-task.web.app"],
     credentials: true,
   })
 );
